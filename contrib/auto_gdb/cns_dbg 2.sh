218f0d8f3de66ad399175c37c50d15a0494d1c7a
CryptoNeverSleeps <36324479+CryptoNeverSleeps@users.noreply.github.com>
2022-01-11 23:50:23 -0500

    Update

--- contrib/auto_gdb/cns_dbg 2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# use testnet settings,  if you need mainnet,  use ~/.cnscore/cnsd.pid file instead
-cns_pid=$(<~/.cnscore/testnet3/cnsd.pid)
-sudo gdb -batch -ex "source debug.gdb" cnsd ${cns_pid}
